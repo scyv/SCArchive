8f3558c83bc1b0d4ea32b1eb4872461282589b48
Yves Schubert <yves.schubert@posteo.de>
2017-07-24 21:33:53 +0200

    initial commit

---
+++ src/main/java/de/scyv/scarchive/server/ScArchiveApplication.java
@@ -0,0 +1,12 @@
+package de.scyv.scarchive.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScArchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScArchiveApplication.class, args);
+	}
+}
